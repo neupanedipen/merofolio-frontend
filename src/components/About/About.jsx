83074871375d29b60732533514faf1fd9a113cdb
Dipendra <deardipen50@gmail.com>
2022-04-27 12:05:14 +0545

    added about page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,37 +1,21 @@
 import React from 'react';
 import styles from './About.module.css'
 import img from '../../images/img1.png'
+import Navbar from '../Navbar'
 
 const About = () => {
     return (
         <>
-            <header class="sub-header">
-                <div class="navbar">
-                    <nav class="nav">
-                        <a href="index.html" id="logo">
-                            <img src="images/header_logo.png" />
-                        </a>
-                        <div class="nav-links" id="navlinks">
-                            <i class="fa fa-times" onclick="hideMenu()"></i>
-
-                            <ul>
-                                <li><a href="index.html">Home</a></li>
-                                <li><a href="">Discussion Forum</a></li>
-                                <li><a href="">Login</a></li>
-                                <li><a href="">Signup</a></li>
-                            </ul>
-                        </div>
-                        <i class="fa fa-bars" onclick="showMenu()"></i>
-                    </nav>
-                </div>
-            </header>
-            <div className={`${styles.textBox} ${styles.text1Box}`}>
+            <header class={styles.subHeader}>
+                <Navbar/>      
+    </header>
+            <div className={`${styles.textBox} ${styles.textBox1}`}>
                 <h1>About Us</h1>
             </div>
             <section className={styles.aboutUs}>
                 <div className={styles.row}>
                     <div className={styles.aboutCol}>
-                        <h2>EASY PROFILE MANAGER</h2>
+                        <h2 className={styles.aboutHead}>EASY PROFILE MANAGER</h2>
                         <p>
                             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae
                             asperiores tenetur tempora, earum dicta ipsam, aperiam, explicabo
